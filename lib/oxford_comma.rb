6723d70033a973d194ea26c9fb868fb156d22f39
Alina Kornilova <alinakornilova78@gmail.com>
2019-09-17 18:37:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join
+  elseif array.length == 2 
+    array.join(" and ")
+  elseif array.length == 3 
+    array.join(",")
+    array.insert(" and")
 end
